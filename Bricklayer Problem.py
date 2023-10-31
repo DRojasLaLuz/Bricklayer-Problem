0507ead6161003eb0fbd9da118ad1bbb5ab872ab
DRojasLaLuz <drojaslaluz@gmail.com>
2023-10-31 12:17:39 -0500

    greedysymmetric added
    Greedy Symmetric algorithm
    Given a value of n (max size of bricks) and a starting configuration, runs the greedy symmetric algorithm to try to find a symmetric solution with the given starting configuration

---
+++ Bricklayer Problem.py
@@ -0,0 +1,123 @@
+def greedysymmetric(n,start_conf):
+    
+    m = int((int(n/2)+1)/2)
+    #ammount of rows = 2*m
+    
+    total = int(n*(n+1)/2)
+    #ammount of slots = total - 1
+
+    blocks = [[1 for column in range(n)] for row in range(m)]
+
+    solution = [[0 for column in range(n)] for row in range(2*m)]
+
+    row_sum = [0 for row in range(2*m)]
+
+    delta = [0 for row in range(2*m)]
+    larger = [0 for row in range(2*m)]
+
+    sol_sum = [[] for row in range(2*m)]
+
+    #function to print information
+    def printdata():
+        print('available blocks:')
+        for i in range(m):
+            print(blocks[i])
+        for i in range(m):
+            print(blocks[-i-1])
+        print('solution:')
+        for i in range(2*m):
+            print(solution[i])
+        print('solution sum:')
+        for i in range(2*m):
+            print(sol_sum[i])
+        #print('row_sum:',row_sum)
+
+    #Set up starting configuration
+    for i in range(m):
+        s = start_conf[i]
+        blocks[i][s-1]= 0
+        solution[i][0] = s
+        solution[-i-1][-1] = s
+        row_sum[i] = s
+        sol_sum[i].append(s)
+        s = start_conf[-i-1]
+        blocks[i][s-1]=0
+        solution[-i-1][0] = s
+        solution[i][-1] = s
+        row_sum[-i-1] = s
+        sol_sum[-i-1].append(s)
+
+    printdata()
+
+    #main loop
+    for slot in range(1,total):
+        #print('slot:',slot) #flag
+        #check if slot is already filled
+        if any(slot in row for row in sol_sum):
+            #print('slot already covered')
+            continue
+        #if slot in row_sum:
+         #   print('slot already covered')
+          #  continue
+        #order largest gaps between rows and slot
+        for row in range(2*m):
+            delta[row] = slot - row_sum[row]
+            larger[row] = delta[row]
+        larger.sort(reverse=True)
+        #check in order if it is possible to add block
+        ended=0
+        for row in range(2*m):
+            cand_block = larger[row]
+            cand_row = delta.index(cand_block)
+            compl_row = 2*m - 1 - cand_row
+            check_row = min(cand_row, compl_row)
+            #print('check_row:',check_row) #flag
+            #print('cand_row',cand_row) #flag
+            #print('cand_block:',cand_block) #flag
+            
+            #check if we need a block bigger than the biggest block
+            if cand_block>n:
+                print('row needs too big of a block. row:',cand_row)
+                break
+            if cand_block<0:
+                print('could not cover slot:',slot)
+                break
+            
+            #if it is possible to add block, add it
+            if blocks[check_row][cand_block-1]:
+                #print('block accepted') #flag
+                blocks[check_row][cand_block-1] = 0
+                ind = solution[cand_row].index(0)
+                cind = n - 1 - ind
+                solution[cand_row][ind] = cand_block
+                solution[compl_row][cind] = cand_block
+                row_sum[cand_row]+=cand_block
+                sol_sum[cand_row].append(row_sum[cand_row])
+                if row_sum[cand_row]==total-row_sum[compl_row]:
+                    res_compl = [total - x for x in sol_sum[cand_row][-2::-1]]
+                    res_cand = [total - x for x in sol_sum[compl_row][-2::-1]]
+                    sol_sum[compl_row].extend(res_compl)
+                    sol_sum[cand_row].extend(res_cand)
+                    
+                ended=1
+                #printdata()
+                break
+            #print('block rejected') #flag
+        #check if slot was not filled
+        if ended==0:
+            print('slot not covered / problem. slot:', slot)
+            printdata()
+            for i in range(2*m):
+               print('row:',i,'block needed:',slot - row_sum[i])
+            print('from the other side:')
+            for i in range(2*m):
+               print('row:',i,'block needed:',total - row_sum[-i-1]-slot)
+            break
+
+    #check solution
+    if ended:
+        print('solution found')
+        printdata()
+
+
+greedysymmetric(10,[1, 2, 4, 5, 7, 8])
